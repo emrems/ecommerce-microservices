8c007005adedc8d4af226a42651e702bec24c607
emre almamis <ealmamis56@gmail.com>
2025-11-27 23:39:19 +0300

    Dtolar yazıldı ve autoMapper implemente edildi

--- Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson;
 
 namespace MultiShop.Catalog.Entities
 {
-    public class ProductImages
+    public class ProductImage
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
@@ -12,6 +12,7 @@ namespace MultiShop.Catalog.Entities
         public string Image2 { get; set; }
         public string Image3 { get; set; }
         public string ProductId { get; set; }
+        [BsonIgnore]
         public Product Product { get; set; }
     }
 }
