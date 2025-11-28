df3a8aee3a19b0c303f80f58c000c62a49e55e96
emre almamis <ealmamis56@gmail.com>
2025-11-28 20:59:50 +0300

    adress silme ve güncelleme yapıldı

---
+++ Services/Orders/Core/MultiShop.Orders.Application/Features/CQRS/Handlers/AdressHandlers/RemoveAdressCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MultiShop.Orders.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Orders.Application.Interfaces;
+using MultiShop.Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Orders.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class RemoveAdressCommandHandler
+    {
+        private readonly IRepository<Adress> _repository;
+
+        public RemoveAdressCommandHandler(IRepository<Adress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAdressCommand command)
+        {
+            var values=await _repository.GetByIdAsync(command.Id);
+            await _repository.DeleteAsync(values);
+        }
+    }
+}
---
+++ Services/Orders/Core/MultiShop.Orders.Application/Features/CQRS/Handlers/AdressHandlers/UpdateAdressCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.Orders.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Orders.Application.Interfaces;
+using MultiShop.Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Orders.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class UpdateAdressCommandHandler
+    {
+        private readonly IRepository<Adress> _repository;
+
+        public UpdateAdressCommandHandler(IRepository<Adress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateAdressCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.AdressId);
+            value.Detail = command.Detail;
+            value.City = command.City;
+            value.District = command.District;
+            value.UserId = command.UserId;
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
