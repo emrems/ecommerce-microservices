0dbca110f892200edefc2fe2fe2d531d26dddbb2
emre almamis <ealmamis56@gmail.com>
2025-11-28 17:09:02 +0300

    discount microservicesine başlandı ayarlar yapıldı tablo oluştruldu

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
+
+namespace MultiShop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DapperContext(DbContextOptions<DapperContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        // Dapper için bağlantı oluşturma
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
