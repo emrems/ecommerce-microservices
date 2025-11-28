4a9d3dd5e8b48fc4c5706528e8f5d3977979512a
emre almamis <ealmamis56@gmail.com>
2025-11-28 20:23:31 +0300

    cqrs klsör yapısı uygulandı

---
+++ Services/Orders/Core/MultiShop.Orders.Application/Features/CQRS/Handlers/AdressHandlers/CreateAdressCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MultiShop.Orders.Application.Features.CQRS.Commands.AdressCommands;
+using MultiShop.Orders.Application.Interfaces;
+using MultiShop.Orders.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Orders.Application.Features.CQRS.Handlers.AdressHandlers
+{
+    public class CreateAdressCommandHandler
+    {
+        private readonly IRepository<Adress> _repository;
+
+        public CreateAdressCommandHandler(IRepository<Adress> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateAdressCommand command)
+        {
+            await _repository.CreateAsync(new Adress
+            {
+                City = command.City,
+                Detail = command.Detail,
+                District = command.Detail,
+                UserId = command.UserId
+            });
+        }
+    }
+}
---
+++ Services/Orders/Core/MultiShop.Orders.Application/Features/CQRS/Queries/AdressQueries/GetAdressByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Orders.Application.Features.CQRS.Queries.AdressQueries
+{
+    public class GetAdressByIdQuery
+    {
+        public int Id { get; set; }
+        public GetAdressByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
