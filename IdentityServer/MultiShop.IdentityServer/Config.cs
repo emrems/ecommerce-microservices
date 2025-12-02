96f7f49916d83f77955007ef64ae10048464f78a
emre almamis <ealmamis56@gmail.com>
2025-12-02 12:23:33 +0300

    Identityserverda config dosyası yazıldı clientlar ve permissionlar verildi

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -9,50 +10,91 @@ namespace MultiShop.IdentityServer
 {
     public static class Config
     {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile(),
-                   };
-
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]// ApiResources propu çağrıldığı zaman her microservice için bir key tanımlayacak
+        {
+            new ApiResource("ResourceCatalog")
             {
-                new ApiScope("scope1"),
-                new ApiScope("scope2"),
-            };
-
-        public static IEnumerable<Client> Clients =>
-            new Client[]
+                Scopes={"CatalogFullPermisson","CatalogReadPermission"}//ResourceCatalog keyine sahip olan biri CatalogFullPermisson scopundaki tüm işlemleri yapabilir. Scopesun içine başka izinler de yazabilirz
+            },
+            new ApiResource("ResourceDiscount")
             {
-                // m2m client credentials flow client
-                new Client
+                Scopes={ "DiscountFullPermission" }
+            },
+            new ApiResource("ResourceOrder")
+            {
+                Scopes = {"OrderFullPermission"}
+            }
+        };
+
+        //IdentityResources ifadesi ile tokenini aldığım kullanıcının 3 değerine erişebilicem token içinde
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            //CatalogFullPermisson tokenına sahip kişi "Full authority for catalog operations" işlemlerinin hepsini yapabilecek
+            new ApiScope("CatalogFullPermisson","Full authority for catalog operations"),
+            new ApiScope("CatalogReadPermission","Reading authority for catalog operations"),
+            new ApiScope("DiscountFullPermission","Reading authority for discount operations"),
+            new ApiScope("OrderFullPermission","Reading authority for order operations")
+        };
+
+        // kendi clientlerimizi oluşturacağız
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            //visitor
+            new Client
+            {
+                ClientId="MultiShopVisitorId",
+                ClientName="MultiShop Visitor User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets =
                 {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
+                    new Secret("multishopsecret".Sha256())
                 },
+                AllowedScopes={ "CatalogReadPermission" }
 
-                // interactive client using code flow + pkce
-                new Client
+            },
+            //Manger
+            new Client
+            {
+                 ClientId="MultiShopManagerId",
+                ClientName="MultiShop Manager User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets =
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope2" }
+                    new Secret("multishopsecret".Sha256())
                 },
-            };
+                AllowedScopes={ "CatalogReadPermission", "CatalogFullPermisson" }
+
+            },
+            //admin
+            new Client
+            {
+                ClientId="MultiShopAdminId",
+                ClientName="MultiShop Admin User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("multishopsecret".Sha256())
+                },
+                AllowedScopes=
+                {   "CatalogReadPermission",
+                    "CatalogFullPermisson",
+                    "DiscountFullPermission",
+                    "OrderFullPermission",
+                    IdentityServerConstants.LocalApi.ScopeName,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime=600//token ömrü
+
+            }
+        };
     }
 }
