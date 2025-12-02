9a6812356c1c465cb75f775dad509d5d283be9b7
emre almamis <ealmamis56@gmail.com>
2025-12-02 11:34:27 +0300

    Identity server için user ekleme controller yazıldı

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto dto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = dto.UserName,
+                Email = dto.Email,
+                Name = dto.UserName,
+                SurName = dto.SurName
+            };
+            var result = await _userManager.CreateAsync(user, dto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("kullanıcı başarılıyla eklendi");
+            }
+            return Ok(result.Errors.Select(x=>x.Description).FirstOrDefault());
+        }
+    }
+}
