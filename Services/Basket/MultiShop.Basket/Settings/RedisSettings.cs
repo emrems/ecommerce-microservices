c62df3e016600331a95dd8fafdf041e89fde3c3c
emre almamis <ealmamis56@gmail.com>
2025-12-04 18:51:57 +0300

    basket redis bağlantısı ve dtolar yazıldı

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Basket.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+       
+
+
+    }
+}
