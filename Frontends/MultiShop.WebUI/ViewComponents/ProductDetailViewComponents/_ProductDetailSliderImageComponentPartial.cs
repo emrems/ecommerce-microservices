08ef3ed016d45d53a95d86e7a71956cb8ba800fd
emre almamis <ealmamis56@gmail.com>
2025-12-08 16:13:53 +0300

    ürün sayfası frondend parçalama ve birleştirme bitti

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailSliderImageComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProductDetailSliderImageComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        } 
+    }
+}
