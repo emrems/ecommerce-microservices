44f720435c4a7b59c00bbf050a91edbb2d9a96f5
emre almamis <ealmamis56@gmail.com>
2025-12-09 12:34:53 +0300

    Admin ürün işlemleri bitti

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Product")]
+    public class ProductController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClient;
+        public ProductController(IHttpClientFactory client)
+        {
+            _httpClient = client;
+        }
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün listesi";
+            ViewBag.v0 = "Ürün işlemleri";
+            var client = _httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7028/api/Products");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();// json gelecek serialize etmek lazım
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("CreateProduct")]
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = "Ürün listesi";
+            ViewBag.v0 = "Ürün işlemleri";
+
+            var client = _httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7028/api/Categories");
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+            List<SelectListItem> categoryValues = (from x in values
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID
+                                                   }).ToList();
+            ViewBag.CategoryValues = categoryValues;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateProduct")]
+        public async Task<IActionResult> CreateProduct(CreateProductDto dto)
+        {
+            var client = _httpClient.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7028/api/Products", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("DeleteProduct/{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            var client = _httpClient.CreateClient();
+            var response = await client.DeleteAsync("https://localhost:7028/api/Products?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("UpdateProduct/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(string id)
+        {
+            ViewBag.v1 = "Ana sayfa";
+            ViewBag.v2 = "Ürünler";
+            ViewBag.v3 = " Ürün güncelleme";
+            ViewBag.v0 = "Ürün işlemleri";
+
+            var client1 = _httpClient.CreateClient();
+            var response1 = await client1.GetAsync("https://localhost:7028/api/Categories");
+            var jsonData1 = await response1.Content.ReadAsStringAsync();
+            var values1 = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData1);
+            List<SelectListItem> categoryValues1 = (from x in values1
+                                                   select new SelectListItem
+                                                   {
+                                                       Text = x.CategoryName,
+                                                       Value = x.CategoryID
+                                                   }).ToList();
+            ViewBag.CategoryValues = categoryValues1;
+
+
+
+
+
+
+            var client = _httpClient.CreateClient();
+            var response = await client.GetAsync("https://localhost:7028/api/Products/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("UpdateProduct/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto dto)
+        {
+
+            var client = _httpClient.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7028/api/Products/", stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index", "Product", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
