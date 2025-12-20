5f98407fb75a0c6d89796730566fecf0a41f03a5
emre almamis <ealmamis56@gmail.com>
2025-12-20 14:57:35 +0300

    IdentityServer tarafında login endpointi oluşturuldu ve test edildi

---
+++ Frontends/MultiShop.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos.RegisterDtos;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public RegisterController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDto dto)
+        {
+            if(dto.Password == dto.ConfirmPassword)
+            {
+                var client = _httpClient.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(dto);
+                StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://localhost:5001/api/Registers", stringContent);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/LoginsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LoginsController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserLogin(UserLoginDto dto)
+        {
+
+            var result = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Ok("giriş başarılı");
+            }
+            return Unauthorized("kullanıcı adı veya şifrea hatalı");
+        }
+          
+    }
+}
